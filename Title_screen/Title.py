53d805f3f1c4e0825b4d80940a0cb1ab2f4e185f
Zaaca-prog <zoggisaac6@gmail.com>
2025-05-31 19:16:28 +0700

    need help bro pls(animation)

--- Title_screen/Title.py
+++ Title_screen/Title.py
@@ -1,15 +1,31 @@
 import pygame
 import TitleData
+import random
+import time
+# Define colors
+colors = {
+    "red": (255, 0, 0), 
+    "green": (0, 255, 0),
+    "blue": (0, 0, 255),
+    "yellow": (255, 255, 0),
+    "purple": (128, 0, 128),
+    }
 
 pygame.init()
-# The Creators
+# For credits
 Creators = [
     "Isaac",
     "Pakthan",
     "Taj"]
+
 IncrementName = pygame.USEREVENT + 1
+ColorChange = pygame.USEREVENT + 2
+# Set up the timer for changing the creator name
 pygame.time.set_timer(IncrementName, 1000)  # Increment every second
+pygame.time.set_timer(ColorChange, 1000)  # Change color every 0.5 seconds
 currentCreator = 0
+Text_color = (255, 255, 255)  # White color for text
+# Title screen
 
 Title = True
 pygame.display.set_caption("Title Screen")
@@ -23,17 +39,19 @@ hoverd = False
 TitleNormal = pygame.transform.scale(pygame.image.load(TitleData.TitleNormal).convert_alpha(),screen.get_size())
 
 #button
-button_image = pygame.image.load(TitleData.Title + r"\Button\NotHoverButton.png").convert_alpha()
+buttons = [TitleData.Buttons + str(i) + ".png" for i in range(3)]
+# Load the button image and create a rect for it
+button_image = pygame.transform.scale(pygame.image.load(TitleData.Title + "\\Button\\" + TitleData.Buttons +"0.png").convert_alpha(), (606, 120))
 button = button_image.get_rect()
 button.center = (ScreenCenter[0], 600)  # Center of the button
 #hoverbutton
-button_hover_image = pygame.image.load(TitleData.Title + r"\Button\HoverButton.png").convert_alpha()
+button_hover_image = pygame.transform.scale(pygame.image.load(TitleData.Title + "\\Button\\" + TitleData.Buttons +"hover.png").convert_alpha(), (606, 120))
 buttonHover = button_hover_image.get_rect()
 buttonHover.center = (ScreenCenter[0], 600)  # Center of the button
 
 #credits
 font = pygame.font.Font(TitleData.Font, 70)
-text = font.render(f"$ {Creators[currentCreator]}", True, (255, 255, 255))
+text = font.render(f"$ {Creators[currentCreator]}", True, (random.choice(list(colors.values()))))
 text_rect = text.get_rect()
 text_rect.topleft = (500, 100)
 
@@ -71,7 +89,6 @@ while Title:
     screen.blit(button_hover_image, buttonHover)
   else:  
     screen.blit(button_image, button)
-    
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       Title = False
@@ -80,6 +97,17 @@ while Title:
       print("Mouse clicked at:", mouse)
       if hoverd:
         print("Button clicked!")
+        for sprite in buttons:
+          print("adding sprite:", sprite)
+          # Load the button image and create a rect for it
+          screen.fill((0, 0, 0))
+          screen.blit(TitleNormal, (0, 0))
+          button_image = pygame.transform.scale(pygame.image.load(TitleData.Title + "\\Button\\" + sprite).convert_alpha(), (606, 120))
+          button = button_image.get_rect()
+          button.center = (ScreenCenter[0], 600)
+          screen.blit(button_image, button)
+          time.sleep(0.5)
+
         #Here you can add the code to start the game or go to the next screen
         Title = False
     elif event.type == IncrementName:
@@ -88,13 +116,20 @@ while Title:
       if currentCreator >= len(Creators):
         currentCreator = 0
       # Update the text to display the current creator
+    elif event.type == ColorChange:
+      # Change the text color randomly
+      Text_color = random.choice(list(colors.values()))
+
+
+
+
   screen.blit(TitleNormal, (0, 0))
   if hoverd:
     screen.blit(button_hover_image, buttonHover)
   else:  
     screen.blit(button_image, button)
   
-  text = font.render(f"$ {Creators[currentCreator]}", True, (255, 255, 255))
+  text = font.render(f"$ {Creators[currentCreator]}", True, Text_color)
   text_rect.topleft = (500, 100)
   screen.blit(text, text_rect)
 
--- Title_screen/TitleData.py
+++ Title_screen/TitleData.py
@@ -1,3 +1,4 @@
 Title = r"C:\Users\ADMIN\Downloads\trash-game-project-main\trash-game-project-main\Title_screen\assets"
 TitleNormal = Title + "\\" + "TrashGame.png"
 Font = Title + "\\" + "ThaleahFat.ttf"
+Buttons = "start_button"
