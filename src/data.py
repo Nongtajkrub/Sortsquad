8f6126f35caaa9d0086aa5d3906ba7f8a402d90f
nongtajkrub <sunanborthwic@gmail.com>
2025-05-31 21:11:07 +0700

    Organize assets file.

--- src/data.py
+++ src/data.py
@@ -1,24 +1,34 @@
-ASSETS_PATH = "/home/nongtajkrub/Coding/Python/trash-game/assets/"
+_ASSETS_PATH = "/home/nongtajkrub/Coding/Python/trash-game/assets"
 
-SKY_IMG_PATH = ASSETS_PATH + "sky.jpg"
+_TESTS_PATH = _ASSETS_PATH + "/tests"
+_ENV_PATH = _ASSETS_PATH + "/env"
+_BINS_PATH = _ASSETS_PATH + "/bins"
+_TRASHES_PATH = _ASSETS_PATH + "/trahse"
+_POWERUP_PATH = _ASSETS_PATH + "/powerup"
+_FONT_PATH = _ASSETS_PATH + "/fonts"
 
-HAZARDOUS_IMG_PATH = ASSETS_PATH + "dangerous_bin.png"
-RECYCLABLE_IMG_PATH = ASSETS_PATH + "recycle_bin.png"
-ORGANIC_IMG_PATH = ASSETS_PATH + "batties_maybe_bin.png"
-GENERAL_IMG_PATH = ASSETS_PATH + "general_bin.png"
+ANIMATION_TEST_IMG_PATH = _TESTS_PATH + "animation_test.png"
 
-APPLE_IMG_PATH = ASSETS_PATH + "apple.png"
-BANANA_IMG_PATH = ASSETS_PATH + "banana.png"
-VEGETABLE_IMG_PATH = ASSETS_PATH + "vegetable.png"
+SKY_IMG_PATH = _ENV_PATH + "/sky.jpg"
+GRASS_IMG_PATH = _ENV_PATH + "/grass.png"
 
-POWER_UP_IMG_PATH = ASSETS_PATH + "powerup.png"
-SHIELD_IMG_PATH = ASSETS_PATH + "shield.png"
-GRASS_IMG_PATH = ASSETS_PATH + "grass.png"
-BATTERY_IMG_PATH = ASSETS_PATH + "battery.png"
-ELECTRONIC_IMG_PATH = ASSETS_PATH + "electronic.png"
-BLEACH_IMG_PATH = ASSETS_PATH + "bleach.png"
+HAZARDOUS_IMG_PATH = _BINS_PATH + "/hazardous/static.png"
+RECYCLABLE_IMG_PATH = _BINS_PATH + "/recyclable/static.png"
+ORGANIC_IMG_PATH = _BINS_PATH + "/organic/static.png"
+GENERAL_IMG_PATH = _BINS_PATH + "/general/static.png"
 
-FONT_PATH = ASSETS_PATH + "font.tff"
+APPLE_IMG_PATH = _TRASHES_PATH + "/organic/apple.png"
+BANANA_IMG_PATH = _TRASHES_PATH + "/organic/banana.png"
+VEGETABLE_IMG_PATH = _TRASHES_PATH + "/organic/vegetable.png"
+
+BATTERY_IMG_PATH = _TRASHES_PATH + "/hazardous/battery.png"
+ELECTRONIC_IMG_PATH = _TRASHES_PATH + "/hazardous/electronic.png"
+BLEACH_IMG_PATH = _TRASHES_PATH + "/hazardous/bleach.png"
+
+POWER_UP_IMG_PATH = _POWERUP_PATH + "/powerup.png"
+SHIELD_IMG_PATH = _POWERUP_PATH + "/shield.png"
+
+FONT_PATH = _FONT_PATH + "/font.tff"
 
 DEFAULT_PLAYER_VEL = 10
 BOOSTED_PLAYER_VEL = 15
