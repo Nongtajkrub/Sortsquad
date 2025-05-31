9ea69711b37a94bfd76a5084b5c3cce837eb4dda
nongtajkrub <sunanborthwic@gmail.com>
2025-05-31 20:40:26 +0700

    Update button. Basic button animation. Screen fade to black when the button is press.

--- Title_screen/TitleData.py
+++
@@ -1,4 +0,0 @@
-Title = r"C:\Users\ADMIN\Downloads\trash-game-project-main\trash-game-project-main\Title_screen\assets"
-TitleNormal = Title + "\\" + "TrashGame.png"
-Font = Title + "\\" + "ThaleahFat.ttf"
-Buttons = "start_button"
