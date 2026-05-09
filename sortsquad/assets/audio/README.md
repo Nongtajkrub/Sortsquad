60c85b375e2faa5bc15bbca3e9bd6f2575d71988
Nongtajkrub <sunanborthwic@gmail.com>
2026-05-09 16:21:19 +0700

    (Rust) Implement basic sprite movement. (Rust) Copy over assets to new rust version.

---
+++ sortsquad/assets/audio/README.md
@@ -0,0 +1,1 @@
+`music.mp3` is a song named `Monplaisir` (Author is unknown).
--- sortsquad/src/main.rs
+++ sortsquad/src/main.rs
@@ -1,5 +1,52 @@
 use bevy::prelude::*;
 
 fn main() {
-  App::new().run();
+    App::new()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_systems(Startup, setup)
+        .add_systems(Update, controllable_movement)
+        .run();
+}
+
+#[derive(Component)]
+struct Controllable;
+
+fn setup(mut commands: Commands, asset: Res<AssetServer>) {
+    commands.spawn(Camera2d);
+
+    commands.spawn((
+        Sprite::from_image(asset.load("bins/general/static.png")),
+        Transform::from_xyz(0., 0., 0.),
+        Controllable,
+    ));
+}
+
+fn controllable_movement(
+    time: Res<Time>,
+    keyboard: Res<ButtonInput<KeyCode>>,
+    mut sprites: Query<&mut Transform, With<Controllable>>
+) {
+    for mut transform in &mut sprites {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard.pressed(KeyCode::KeyW) {
+            direction.y += 1.;
+        }
+        if keyboard.pressed(KeyCode::KeyS) {
+            direction.y -= 1.;
+        }
+        
+        if keyboard.pressed(KeyCode::KeyD) {
+            direction.x += 1.;
+        }
+        if keyboard.pressed(KeyCode::KeyA) {
+            direction.x -= 1.;
+        }
+
+        if direction != Vec3::ZERO {
+            direction = direction.normalize();
+        }
+
+        transform.translation += direction * 400. * time.delta_secs();
+    }
 }
